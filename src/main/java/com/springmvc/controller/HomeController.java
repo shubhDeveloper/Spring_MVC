9c6879dbaf57c7b80760d252754fead1dcbbd0bf
shubham panchal <75712185+shubhDeveloper@users.noreply.github.com>
2021-01-30 12:29:10 +0530

    Added ModelAndView and jstl

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.springmvc.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.coyote.http2.Setting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -27,9 +30,33 @@ public class HomeController {
 
 	
 	@RequestMapping("/about")
-	public String about() {
+	public ModelAndView about() {
 		System.out.println("About controller running");
-		return "about";
+		
+		//creating ModelAndView object 
+		ModelAndView modelAndView = new ModelAndView();
+		
+		//Setting the data 
+		modelAndView.addObject("fname", "Rakesh");
+		modelAndView.addObject("lname", "Nema");
+		LocalDateTime now = LocalDateTime.now();
+		modelAndView.addObject("time",now);
+	    
+		//Added List
+		List<String> student = new ArrayList<String>();
+		student.add("rohan");
+		student.add("lokesh");
+		student.add("vihan");
+		student.add("harry");
+		student.add("vikas");
+		
+ 		//Setting the list
+		modelAndView.addObject("students", student);
+		
+		//Setting the view name
+		modelAndView.setViewName("about");
+		
+		return modelAndView;
 		
 	}
 }
--- src/main/webapp/WEB-INF/views/about.jsp
+++ src/main/webapp/WEB-INF/views/about.jsp
@@ -1,5 +1,10 @@
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.List"%>
+<%@page import="java.time.LocalDateTime"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@page isELIgnored="false"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +12,30 @@
 <title>About page</title>
 </head>
 <body>
-<h1>Hello this is about page</h1>
+	<h1>Hello this is about page</h1>
+
+	<%
+	/* String name1 = (String)request.getAttribute("fname"); */
+	/* String name2 = (String) request.getAttribute("lname"); */
+	LocalDateTime date = (LocalDateTime) request.getAttribute("time");
+	%>
+
+    <!-- Using jstl -->
+	<c:forEach var="item" items="${students }">
+		<%-- <h1>students is = ${item }</h1> --%>
+		<h1> <c:out value="${item}"></c:out> </h1>
+	</c:forEach>
+
+
+	<!-- Using Expresion language -->
+	<h1>first name is = ${fname}</h1>
+
+
+	<%-- <h1>last name is = <%= name2 %></h1> --%>
+	<h1>last name is = ${lname}</h1>
+	<h1>
+		Data Time is =
+		<%=date%></h1>
+
 </body>
 </html>
